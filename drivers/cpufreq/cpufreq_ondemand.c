609e05af0def8fde5c52b55b92360ae2ca928952
UpInTheAir <upintheair.skyhigh@gmail.com>
2017-01-13 16:12:49 +0700

    cpufreq: apply CPUFREQ_RELATION_C to all CPU freq governors
    Signed-off-by: UpInTheAir <upintheair.skyhigh@gmail.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -286,7 +286,7 @@ static unsigned int powersave_bias_target(struct cpufreq_policy *policy,
 	freq_lo = dbs_info->freq_table[index].frequency;
 	index = 0;
 	cpufreq_frequency_table_target(policy, dbs_info->freq_table, freq_avg,
-			CPUFREQ_RELATION_L, &index);
+			CPUFREQ_RELATION_C, &index);
 	freq_hi = dbs_info->freq_table[index].frequency;
 
 	/* Find out how long we have to be in hi and lo freqs */
@@ -733,7 +733,7 @@ static void dbs_freq_increase(struct cpufreq_policy *p, unsigned int freq)
 		freq = powersave_bias_target(p, freq, CPUFREQ_RELATION_H);
 
 	__cpufreq_driver_target(p, freq, dbs_tuners_ins.powersave_bias ?
-			CPUFREQ_RELATION_L : CPUFREQ_RELATION_H);
+			CPUFREQ_RELATION_C : CPUFREQ_RELATION_H);
 }
 
 static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
@@ -902,12 +902,12 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 			if (!dbs_tuners_ins.powersave_bias) {
 				__cpufreq_driver_target(policy, freq_next,
-						CPUFREQ_RELATION_L);
+						CPUFREQ_RELATION_C);
 			} else {
 				int freq = powersave_bias_target(policy, freq_next,
-						CPUFREQ_RELATION_L);
+						CPUFREQ_RELATION_C);
 				__cpufreq_driver_target(policy, freq,
-					CPUFREQ_RELATION_L);
+					CPUFREQ_RELATION_C);
 			}
 		}
 	} else {
@@ -931,12 +931,12 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 			if (!dbs_tuners_ins.powersave_bias) {
 				__cpufreq_driver_target(policy, freq_next,
-						CPUFREQ_RELATION_L);
+						CPUFREQ_RELATION_C);
 			} else {
 				int freq = powersave_bias_target(policy, freq_next,
-						CPUFREQ_RELATION_L);
+						CPUFREQ_RELATION_C);
 				__cpufreq_driver_target(policy, freq,
-					CPUFREQ_RELATION_L);
+					CPUFREQ_RELATION_C);
 			}
 		}
 	}
@@ -1125,7 +1125,7 @@ static int __ref cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				policy->max, CPUFREQ_RELATION_H);
 		else if (policy->min > this_dbs_info->cur_policy->cur)
 			__cpufreq_driver_target(this_dbs_info->cur_policy,
-				policy->min, CPUFREQ_RELATION_L);
+				policy->min, CPUFREQ_RELATION_C);
 		mutex_unlock(&this_dbs_info->timer_mutex);
 		break;
 	}
